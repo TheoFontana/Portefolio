40da55fd26b49d9e8693c2224e5a59d0a8051ad4
Théo Fontana <tfontana@etud.insa-toulouse.fr>
2023-09-27 11:28:14 +0200

    🎨 Try new UI stuff

---
+++ src/animation.js
@@ -0,0 +1,19 @@
+const cards = document.getElementsByClassName("card-effect")
+console.log(cards)
+for (let card of cards) {
+    card.addEventListener('mousemove', e => {
+        const el = e.target.closest(".card-effect")
+        const rect = el.getBoundingClientRect();
+
+        const x = -1 + 2 * ((e.clientX - rect.left) / el.offsetWidth)
+        const y = 1 - 2 * ((e.clientY - rect.top) / el.offsetHeight)
+        const dist = Math.sqrt( x** 2 + y ** 2)
+        // el.style.transform = `rotate3D(${y}, ${x}, 0, ${-10 * dist}deg)`
+        // console.log(dist)
+    })
+    card.addEventListener('mouseleave', e => {
+        const el = e.target.closest(".card-effect")
+        el.style.transform = `rotate3D(0,0,0,0deg)`
+    })
+}
+
--- src/three.js
+++ src/three.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {FontLoader} from 'three/addons/loaders/FontLoader.js';
+import {TextGeometry} from 'three/addons/geometries/TextGeometry.js';
 import * as  dat from 'lil-gui'
 
 /**
@@ -15,7 +15,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0x050229 );
+scene.background = new THREE.Color(0x050229);
 
 /**
  * Lights
@@ -45,7 +45,7 @@ scene.add(SpotLight2)
 // scene.add(spotLightHelper2)
 
 const main_key_words = ['5G/6G ', 'Sclicing ', 'Autonomus Computing  ', 'QoS ']
-const secondary_key_words = ['DevOps ','Cloud ', 'Frontend ']
+const secondary_key_words = ['DevOps ', 'Cloud ', 'Frontend ']
 
 const text_material = new THREE.MeshStandardMaterial({
     color: 0xffffff
@@ -68,7 +68,7 @@ const create3DLetters = (text_material, words) => {
                     bevelSegments: 5,
                 });
                 const letter_mesh = new THREE.Mesh(letter_geometry, text_material);
-                letter_mesh.position.y = - .5
+                letter_mesh.position.y = -.5
                 scene.add(letter_mesh);
                 letters.push(letter_mesh)
             }
@@ -77,15 +77,19 @@ const create3DLetters = (text_material, words) => {
     return letters;
 }
 
-const main_letters = create3DLetters( text_material, main_key_words);
-const secondary_letters = create3DLetters( text_material, secondary_key_words);
+const main_letters = create3DLetters(text_material, main_key_words);
+const secondary_letters = create3DLetters(text_material, secondary_key_words);
 /**
  * Objects
  */
 // Material
-const material = new THREE.MeshStandardMaterial({ color: 0x2916F3 })
+const material = new THREE.MeshStandardMaterial({color: 0x2916F3})
 material.roughness = 0.5
 material.metalness = 0.3
+
+const mini_sphere_material = new THREE.MeshStandardMaterial({color: 0xffffff})
+mini_sphere_material.roughness = 0.5
+mini_sphere_material.metalness = 0.3
 // gui.add(material, 'metalness').min(0).max(1).step(0.0001)
 // gui.add(material, 'roughness').min(0).max(1).step(0.0001)
 
@@ -99,7 +103,22 @@ const small_sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.25, 32, 32),
     material
 )
-scene.add(big_sphere,small_sphere)
+scene.add(big_sphere, small_sphere)
+
+for (let i = 0; i < 15; i++) {
+    for (let j = 0; j < 10; j++) {
+        const mini_sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(0.01, 16, 16),
+            mini_sphere_material
+        )
+        mini_sphere.position.x = -1 + 2 * i / 10
+        mini_sphere.position.z = -1 + j / 2
+        mini_sphere.position.y = -.2
+
+        scene.add(mini_sphere)
+    }
+
+}
 
 /**
  * Sizes
@@ -109,8 +128,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -128,8 +146,7 @@ window.addEventListener('resize', () =>
         big_sphere.position.y = -.5
         small_sphere.position.y = -.5
         big_sphere_center = 0
-    }
-    else {
+    } else {
         camera.position.y = 1
         big_sphere.position.y = 0
         small_sphere.position.y = 0
@@ -142,7 +159,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
+// camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 3
 scene.add(camera)
@@ -176,31 +193,30 @@ if (window.innerWidth < 425) {
  */
 const clock = new THREE.Clock()
 const spacing = 0.15
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    big_sphere.position.x = big_sphere_center + Math.cos(0.5 * elapsedTime)/10*Math.PI
-    big_sphere.position.z = Math.sin(0.5 * elapsedTime)/7*Math.PI
+    big_sphere.position.x = big_sphere_center + Math.cos(0.5 * elapsedTime) / 10 * Math.PI
+    big_sphere.position.z = Math.sin(0.5 * elapsedTime) / 7 * Math.PI
 
     small_sphere.position.x = big_sphere.position.x + 1.2 * Math.cos(0.8 * elapsedTime)
     small_sphere.position.z = big_sphere.position.z - 1.5 * Math.sin(0.8 * elapsedTime)
 
     if (main_letters.length && secondary_letters.length) {
         main_letters.forEach((letter, index) => {
-            letter.position.x =  big_sphere.position.x + 0.54 * Math.cos(0.8 * elapsedTime - spacing * index)
-            letter.position.z =  big_sphere.position.z + 0.54 * Math.sin(0.8 * elapsedTime - spacing * index)
-            letter.position.y =  big_sphere.position.y
+            letter.position.x = big_sphere.position.x + 0.54 * Math.cos(0.8 * elapsedTime - spacing * index)
+            letter.position.z = big_sphere.position.z + 0.54 * Math.sin(0.8 * elapsedTime - spacing * index)
+            letter.position.y = big_sphere.position.y
 
-            letter.rotation.y =  Math.PI/2 - 0.8 * elapsedTime + spacing * index
+            letter.rotation.y = Math.PI / 2 - 0.8 * elapsedTime + spacing * index
         })
-        secondary_letters.forEach((letter, index) =>{
-            letter.position.x =  small_sphere.position.x + 0.3 * Math.cos(1 * elapsedTime - 1.8 * spacing * index)
-            letter.position.z =  small_sphere.position.z + 0.3 * Math.sin(1 * elapsedTime - 1.8 * spacing * index)
-            letter.position.y =  small_sphere.position.y
+        secondary_letters.forEach((letter, index) => {
+            letter.position.x = small_sphere.position.x + 0.27 * Math.cos(1 * elapsedTime - 1.8 * spacing * index)
+            letter.position.z = small_sphere.position.z + 0.27 * Math.sin(1 * elapsedTime - 1.8 * spacing * index)
+            letter.position.y = small_sphere.position.y
 
-            letter.rotation.y =  Math.PI/2 - 1 * elapsedTime + 1.8 * spacing * index
+            letter.rotation.y = Math.PI / 2 - 1 * elapsedTime + 1.8 * spacing * index
         })
     }
 
